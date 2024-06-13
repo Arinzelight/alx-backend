4bfa016bd71291814dff0e5a8b3115d1f46bbf67
Arinzelight <arinzelight2@gmail.com>
2024-06-13 01:03:48 +0100

    Node Redis client and basic operations

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -1,29 +1,26 @@
-import { createClient, print } from "redis";
+#!/usr/bin/yarn dev
+import { createClient, print } from 'redis';
 
 const client = createClient();
 
-client
-  .on("connect", () => {
-    console.log("Redis client connected to the server");
-  })
-  .on("error", (error) => {
-    console.log(`Redis client not connected to the server: ${error}`);
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+const setNewSchool = (schoolName, value) => {
+  client.SET(schoolName, value, print);
+};
+
+const displaySchoolValue = (schoolName) => {
+  client.GET(schoolName, (_err, reply) => {
+    console.log(reply);
   });
+};
 
-function setNewSchool(schoolName, value) {
-  client.set(schoolName, value, print);
-}
-
-function displaySchoolValue(schoolName) {
-  client.get(schoolName, (error, result) => {
-    if (error) {
-      console.log(error);
-      throw error;
-    }
-    console.log(result);
-  });
-}
-
-displaySchoolValue("Holberton");
-setNewSchool("HolbertonSanFrancisco", "100");
-displaySchoolValue("HolbertonSanFrancisco");
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
