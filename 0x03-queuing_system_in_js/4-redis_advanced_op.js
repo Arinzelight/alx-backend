a7ac2792aa25107df6994d98eead7926eb55f2a6
Arinzelight <arinzelight2@gmail.com>
2024-06-13 01:05:05 +0100

    Node Redis client and advanced operations

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -1,26 +1,36 @@
-import { createClient, print } from "redis";
+#!/usr/bin/yarn dev
+import { createClient, print } from 'redis';
 
 const client = createClient();
 
-client
-  .on("connect", () => {
-    console.log("Redis client connected to the server");
-  })
-  .on("error", (error) => {
-    console.log(`Redis client not connected to the server: ${error}`);
-  });
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
 
-client.hset('HolbertonSchools', 'Portland', '50', print);
-client.hset('HolbertonSchools', 'Seattle', '80', print);
-client.hset('HolbertonSchools', 'New York', '20', print);
-client.hset('HolbertonSchools', 'Bogota', '20', print);
-client.hset('HolbertonSchools', 'Cali', '40', print);
-client.hset('HolbertonSchools', 'Paris', '2', print);
+const updateHash = (hashName, fieldName, fieldValue) => {
+  client.HSET(hashName, fieldName, fieldValue, print);
+};
 
-client.hgetall('HolbertonSchools', (error, result) => {
-    if (error) {
-        console.log(error);
-        throw error;
-    }
-    console.log(result);
+const printHash = (hashName) => {
+  client.HGETALL(hashName, (_err, reply) => console.log(reply));
+};
+
+function main() {
+  const hashObj = {
+    Portland: 50,
+    Seattle: 80,
+    'New York': 20,
+    Bogota: 20,
+    Cali: 40,
+    Paris: 2,
+  };
+  for (const [field, value] of Object.entries(hashObj)) {
+    updateHash('HolbertonSchools', field, value);
+  }
+  printHash('HolbertonSchools');
+}
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+  main();
 });
