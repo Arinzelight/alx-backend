5cbc4ab701057d11b5985435dc4098575be0e703
Arinzelight <arinzelight2@gmail.com>
2024-06-13 01:03:11 +0100

    Node Redis Client

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,11 +1,12 @@
-import { createClient } from "redis";
+#!/usr/bin/yarn dev
+import { createClient } from 'redis';
 
 const client = createClient();
 
-client
-  .on("connect", () => {
-    console.log("Redis client connected to the server");
-  })
-  .on("error", (error) => {
-    console.log(`Redis client not connected to the server: ${error}`);
-  });
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
